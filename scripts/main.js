9f1f4da74d08525b418ed8e0270ecb4fc28b67b1
bastianSalinas <bast.s.rojas@gmail.com>
2023-06-04 19:29:13 -0400

    Terminando la seleccion de avertura

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,41 @@
 if (window.location.href.indexOf('index.html') > -1) {
 
-    let text = document.getElementById('txtPara');
-    let leaf = document.getElementById('leaf');
-    let hill1 = document.getElementById('hill1');
-    let hill2 = document.getElementById('hill2');
-    let hill3 = document.getElementById('hill3');
-    let hill4 = document.getElementById('hill4');
-    let hill5 = document.getElementById('hill5');
+  let text = document.getElementById('txtPara');
+  let leaf = document.getElementById('leaf');
+  let hill1 = document.getElementById('hill1');
+  let hill2 = document.getElementById('hill2');
+  let hill3 = document.getElementById('hill3');
+  let hill4 = document.getElementById('hill4');
+  let hill5 = document.getElementById('hill5');
 
-    window.addEventListener('scroll', () =>{
-        let value = window.scrollY;
-        text.style.marginTop = value * 2.5 + 'px';
-        leaf.style.left = value * 1.5 + 'px';
-        hill5.style.left = value * 1.5 + 'px';
-        hill4.style.left = value * -0.3 + 'px';
-        hill3.style.left = value * 0.4 + 'px';
-    });
+  window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    text.style.marginTop = value * 2.5 + 'px';
+    leaf.style.left = value * 1.5 + 'px';
+    hill5.style.left = value * 1.5 + 'px';
+    hill4.style.left = value * -0.3 + 'px';
+    hill3.style.left = value * 0.4 + 'px';
+  });
 }
+if (window.location.href.indexOf('parques.html') > -1) {
 
-let parques = [];
-//Cargamos parques desde el archivo parques.json
-fetch("../scripts/parques.json")
+  let parques = [];
+  //Cargamos parques desde el archivo parques.json
+  fetch("../scripts/parques.json")
     .then(response => response.json())
     .then(data => {
-        parques = data;
-        cargarParques(parques);
+      parques = data;
+      cargarParques(parques);
     })
 
-const contenedorParques = document.querySelector("#contenedor-parques"); 
-const botonesCategoria = document.querySelectorAll(".boton-categoria"); 
-let botonSeleccion = document.querySelectorAll(".parque-seleccion"); 
-const numero = document.querySelector("#numero");
+  const contenedorParques = document.querySelector("#contenedor-parques");
+  const botonesCategoria = document.querySelectorAll(".boton-categoria");
+  let botonSeleccion = document.querySelectorAll(".parque-seleccion");
+  const numero = document.querySelector("#numero");
 
-let parquesEnAventura = [];
-// Cargar datos del almacenamiento local al iniciar la página
-window.addEventListener('DOMContentLoaded', () => {
+  let parquesEnAventura = [];
+  // Cargar datos del almacenamiento local al iniciar la página
+  window.addEventListener('DOMContentLoaded', () => {
     const parquesGuardados = localStorage.getItem('parques-en-aventura');
     if (parquesGuardados) {
       parquesEnAventura = JSON.parse(parquesGuardados);
@@ -42,15 +43,15 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-function cargarParques(parquesSeleccionados) {
+  function cargarParques(parquesSeleccionados) {
 
     contenedorParques.innerHTML = "";
 
     parquesSeleccionados.forEach(parque => {
 
-        const div = document.createElement("div");
-        div.classList.add("col");
-        div.innerHTML = `
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
                 <div class="card">
                     <img src="${parque.imagen}" class="img-parque" alt="${parque.titulo}">
                     <div class="card-body">
@@ -67,29 +68,29 @@ function cargarParques(parquesSeleccionados) {
                     </div>
                 </div>
         `;
-        contenedorParques.append(div);
+      contenedorParques.append(div);
     });
     clicBotonAgregar();
-}
+  }
 
-cargarParques(parques);
+  cargarParques(parques);
 
-botonesCategoria.forEach(boton => {
+  botonesCategoria.forEach(boton => {
     boton.addEventListener("click", (e) => {
 
-        botonesCategoria.forEach(boton => boton.classList.remove("active"));
-        e.currentTarget.classList.add("active");
+      botonesCategoria.forEach(boton => boton.classList.remove("active"));
+      e.currentTarget.classList.add("active");
 
-        if (e.currentTarget.id !== "todos") {
-            const categoriaSeleccionada = parques.filter(parque => parque.categoria.id === e.currentTarget.id);
-            cargarParques(categoriaSeleccionada);
-        } else {
-            cargarParques(parques);
-        }
+      if (e.currentTarget.id !== "todos") {
+        const categoriaSeleccionada = parques.filter(parque => parque.categoria.id === e.currentTarget.id);
+        cargarParques(categoriaSeleccionada);
+      } else {
+        cargarParques(parques);
+      }
     });
-});
+  });
 
-function clicBotonAgregar() {
+  function clicBotonAgregar() {
     botonSeleccion = document.querySelectorAll(".parque-seleccion");
     botonSeleccion.forEach(boton => {
       boton.addEventListener("click", (e) => {
@@ -119,3 +120,4 @@ function clicBotonAgregar() {
   function guardarParquesEnAventura() {
     localStorage.setItem('parques-en-aventura', JSON.stringify(parquesEnAventura));
   }
+}
